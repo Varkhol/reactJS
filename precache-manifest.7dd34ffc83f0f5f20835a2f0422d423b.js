d6e404ab3124ce3b5881688ed1d5ce1c32413dcb
varkhol.yuliia <juljailchenko@gmail.com>
2020-09-21 00:34:31 +0300

    Updates

---
+++ precache-manifest.7dd34ffc83f0f5f20835a2f0422d423b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908c1a07de18c265afeeb455355cd6b1",
+    "url": "/reactJS/index.html"
+  },
+  {
+    "revision": "8980b03ea225f3f21aa4",
+    "url": "/reactJS/static/css/main.b4fc5993.chunk.css"
+  },
+  {
+    "revision": "a82c789e4a99d29bd732",
+    "url": "/reactJS/static/js/2.57cf4e78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactJS/static/js/2.57cf4e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8980b03ea225f3f21aa4",
+    "url": "/reactJS/static/js/main.2283dfdb.chunk.js"
+  },
+  {
+    "revision": "41b182a0eeaf91d1b5bc",
+    "url": "/reactJS/static/js/runtime-main.7b55f495.js"
+  }
+]);
